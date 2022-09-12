b2df75a8052d483de3824f85709de75853a9605a
yared <yb12ybk@gmail.com>
2022-09-12 13:00:51 -0700

    test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,7 +9,6 @@
  */
 int main(void)
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);
-return (0); /* return 0*/
+printf("with proper grammar, but the outcome is a piece of art,");
+Return (0); /* return 0*/
 }
