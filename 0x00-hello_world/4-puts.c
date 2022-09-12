9d0eed719d3aa7e476a89b057f942499628c48b1
yared <yb12ybk@gmail.com>
2022-09-12 12:17:56 -0700

    test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * Description: 'how to use puts()'
+ */
+/**
+ * main - this is the main function
+ */
 int main(void)
 {
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
 puts(Mystr);
-return (0);
+return (0); /* return 0*/
 }
