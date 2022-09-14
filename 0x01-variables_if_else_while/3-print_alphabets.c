bf10777c8307e0f229bf4248a89a7f79025fc066
yared <yb12ybk@gmail.com>
2022-09-14 12:25:03 -0700

    test

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-for (char  i = a; i <= z; i++)
+for (char  i = 'a'; i <= 'z'; i++)
 putchar(i);
-for (char  i = A; i <= Z; i++)
+for (char  i = 'A'; i <= 'Z'; i++)
 putchar(i);
 putchar(n);
 return (0);
