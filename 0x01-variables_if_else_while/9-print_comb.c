d5aec6e894f41e1c51f2a3c8aede55d3f7196016
yared <yb12ybk@gmail.com>
2022-09-14 14:41:33 -0700

    test

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print alphabets
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+putchar(48);
+for (i = 49; i <= 57; i++)
+{
+putchar(44);
+putchar(32);
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
