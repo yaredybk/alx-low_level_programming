5babfb0aa0e29389d81fc9a76105337d0e180375
yared <yb12ybk@gmail.com>
2022-09-25 13:31:56 -0700

    test

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,13 +7,13 @@ int main(void)
 {
 int i;
 int j;
-for(i = 48; i<= 57; i++)
+for (i = 48; i <= 57; i++)
 {
-for (j = i+1; j <= 57; j++)
+for (j = i + 1; j <= 57; j++)
 {
 putchar(i);
 putchar(j);
-if(i!=56)
+if (i != 56)
 {
 putchar(44);
 putchar(32);
