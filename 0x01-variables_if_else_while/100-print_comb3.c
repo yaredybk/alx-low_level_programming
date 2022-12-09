b329ed1686bc70c9f7690e41b9d65f960c22bf5f
yaredybk <yb12ybk@gmail.com>
2022-12-09 13:32:08 -0800

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,22 +7,19 @@ int main(void)
 {
 	int i;
 	int j;
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i <= 56; i++)
 	{
 		for (j = i + 1; j <= 57; j++)
 		{
-			putchar(i);
-			putchar(j);
-			if (i != 56)
+			if (i > 48)
 			{
 				putchar(44);
 				putchar(32);
 			}
-			else
-			{
-				putchar('\n');
-			}
+			putchar(i);
+			putchar(j);
 		}
 	}
+	putchar('\n');
 return (0);
 }
