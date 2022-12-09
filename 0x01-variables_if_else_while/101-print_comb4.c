5bb43e49a57fcba6d57a07298811f9bbc78a2f16
yaredybk <yb12ybk@gmail.com>
2022-12-09 13:41:04 -0800

    hot fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,24 +10,24 @@ int j;
 int k;
 for (i = 48; i <= 57; i++)
 {
-for (j = i + 1; j <= 57; j++)
-{
-for (k = j + 1; k <= 57; k++)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i != 55)
-{
-putchar(44);
-putchar(32);
-}
-else
-{
-putchar('\n');
-}
-}
-}
+	for (j = i + 1; j <= 57; j++)
+	{
+		for (k = j + 1; k <= 57; k++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(k);
+			if (i != 55)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			else
+			{
+				putchar('\n');
+			}
+		}
+	}
 }
 return (0);
 }
