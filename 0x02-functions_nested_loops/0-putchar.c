89e94c76b82cc8ec1e6019dcdcaad0ce2925134b
yaredybk <yb12ybk@gmail.com>
2022-12-13 11:21:01 -0800

    test

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<main.h>
 /*
  * main: prints "_putchar"
  *
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	putchar();
 	return(0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/*
+ * putchar: prints "_putchar"
+ *
+ * Returns: always 0
+ */
+int putchar(void)
+{
+	printf("_putchar");
+}
