91408a1c4595384809114457d8d9787a12a6c05f
yaredybk <yb12ybk@gmail.com>
2022-12-14 14:34:39 -0800

    hot fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * followed by a new line
+ * @n: input number to start counting
+ */
+void print_to_98(int n)
+{
+	do {
+		_putchar(n + '0');
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (n > 98)
+			n--;
+		else if (n < 98)
+			n--;
+	} while (n != 98)
+	return (0);
+}
