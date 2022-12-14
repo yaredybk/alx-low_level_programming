3862f24b2fceb8fd43e6eb3dc3f3ca4e2c2c3605
yaredybk <yb12ybk@gmail.com>
2022-12-14 12:09:18 -0800

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,22 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf('-');
+		_putchar('-');
 		return (-1);
 	}
 	else if(n > 0)
 	{
-		printf('+');
+		_putchar('+');
 		return (1);
 	} 
 	else if(n == 0)
 	{
-		printf('0');
+		_putchar('0');
 		return (0);
 	}
+	else
+	{
+		_putchar('-');
+		return ('/')'
+			}
 }
