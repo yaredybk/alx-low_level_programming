24b5885eea1120071d18159cb014cf73ff169f48
yaredybk <yb12ybk@gmail.com>
2022-12-14 05:50:57 -0800

    hot fix

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: target character
+ * Return: 1 if c is a letter, lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+	int i = isalpha(c);
+	if(i)
+		return (1);
+	else
+		return (0);
+}	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<ctype.h>
 #ifndef mainHeader
 #define mainHeader
 
