8f1bf95eba86196556830e45fd3b5c96442a32d7
yaredybk <yb12ybk@gmail.com>
2022-12-14 12:44:57 -0800

    hot fix

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: input number 
+ * Return: the value of the last digit
+ */
+int print_last_digit(int i)
+{
+	return((abs(i))%10);
+}
