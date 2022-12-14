e411afa3d23ec656bce03f737d7088195ded44e1
yaredybk <yb12ybk@gmail.com>
2022-12-14 12:23:30 -0800

    hot fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,9 +3,9 @@
 /**
  * print_sign - prints the sign of a number
  * @n: input number for testing
- * Returns: 1 and prints + if n is greater than zero
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zeroReturns 1 and prints + if n is greater than zero
+ * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -14,12 +14,12 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	else if(n > 0)
+	else if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} 
-	else if(n == 0)
+	}
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
@@ -27,6 +27,6 @@ int print_sign(int n)
 	else
 	{
 		_putchar('-');
-		return ('/')'
+		return ('/');
 	}
 }
