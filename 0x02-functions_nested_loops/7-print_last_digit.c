1152d805a8922c242b9a3a0f8a3936924b03deb9
yaredybk <yb12ybk@gmail.com>
2022-12-14 12:46:23 -0800

    hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,10 +2,10 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @i: input number 
+ * @i: input number
  * Return: the value of the last digit
  */
 int print_last_digit(int i)
 {
-	return((abs(i))%10);
+	return ((abs(i)) % 10);
 }
