b00ed5a09a9c9f7c5b1ff646237819ce1f99b6f5
yaredybk <yb12ybk@gmail.com>
2022-12-15 13:24:21 -0800

    hot fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include"main.h"
 
 /**
- * prints the numbers, from 0 to 9, followed by a new line.
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  * Return: always 0
  */
 int print_numbers(void)
 {
 	int i;
 
-	for(i = 48; i <= 58; i++)
+	for (i = 48; i <= 58; i++)
 	{
 		_putchar(i);
 	}
