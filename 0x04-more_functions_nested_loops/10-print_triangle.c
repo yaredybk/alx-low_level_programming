e82a25813edfedb76f75324057934db94a51f865
yaredybk <yb12ybk@gmail.com>
2022-12-15 15:11:46 -0800

    hot fix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 	if (size > 0)
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < (size - i); j++)
+			for (j = 1; j < (size - i); j++)
 			{
 				_putchar(' ');
 			}	
