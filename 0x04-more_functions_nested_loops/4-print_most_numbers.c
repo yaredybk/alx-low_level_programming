c351cfeef65331b19ea3fcadc51320dc8839cbd9
yaredybk <yb12ybk@gmail.com>
2022-12-15 12:45:51 -0800

    more_functions_nested_loops tasks #ALX

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 58; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		else
+			_putchar(i);
+	}
+}
