823117e13b2ff257cf56f59feb6b996114f04627
yaredybk <yb12ybk@gmail.com>
2022-12-15 14:33:39 -0800

    hot fix

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,12 +7,15 @@
 void print_square(int size)
 {
 	int i;
+	int j;
 
 	if (size > 0)
 		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
-			_putchar('\n');
+			for (j = 0; j < size; i++)
+			{
+				_putchar('#');
+			}
 		}
 	else
 		_putchar('\n');
