c0e8ddf020520aa1c81fda2b0d76f880f14497f4
yaredybk <yb12ybk@gmail.com>
2022-12-15 05:39:00 -0800

    hot fix

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,9 @@
+#include"main.h"
+/**
+ * main - test the function "positive_or_negative" is correct when tested at 0
+ * Return: 1 if the function is correct else Return 0
+ */
+int main(void)
+{
+	printf("0%d",positive_or_negative(0));
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+#ifndef MAIN_H
+#define MAIN_H
+
+void positive_or_negative(int i);
+
+#endif
