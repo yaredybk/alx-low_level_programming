1fa7e89ef9671552b8b0f0dbbd7ed0a92e0dbd18
yaredybk <yb12ybk@gmail.com>
2022-12-15 15:06:08 -0800

    more_functions_nested_loops tasks #ALX

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_square - prints a square, followed by a new line.
+ * @size: input number
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < (size - i); j++)
+			{
+				_putchar(' ');
+			}	
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+				_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
