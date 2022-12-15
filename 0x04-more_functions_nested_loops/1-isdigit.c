8c0858ef33b9bb6312da7f81dff94fc45681a002
yaredybk <yb12ybk@gmail.com>
2022-12-15 13:09:51 -0800

    hot fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,12 @@
 #include"main.h"
 
 /**
- * _isupper - 
+ * _isupper - checks for a digit (0 through 9)
  * @c: input character to checked
  * Return: 1 if c is a digit
  * 0 otherwise
  */
 int _isupper(int c)
 {
-        if (isdigit(c))
-                return (1);
-        else
-                return (0);
+	return (isdigit(c));
 }
