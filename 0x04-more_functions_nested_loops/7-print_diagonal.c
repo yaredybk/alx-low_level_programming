c4c0d855d2abe144f14ad9ba05f3d5bcc26047fd
yaredybk <yb12ybk@gmail.com>
2022-12-15 14:11:33 -0800

    more_functions_nested_loops tasks #ALX

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: input number
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
