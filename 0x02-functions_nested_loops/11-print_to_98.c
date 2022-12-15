d787485f3c22b8b1ea3c4454173382559f3cb446
yaredybk <yb12ybk@gmail.com>
2022-12-14 21:54:58 -0800

    hot fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,7 +6,7 @@
  */
 void print_to_98(int n)
 {
-	printf("%d",n);
+	printf("%d", n);
 	while (n != 98)
 	{
 		if (n != 98)
@@ -17,7 +17,7 @@ void print_to_98(int n)
 			n--;
 		else if (n < 98)
 			n++;
-		printf("%d",n);
+		printf("%d", n);
 	}
 	printf("\n");
 }
