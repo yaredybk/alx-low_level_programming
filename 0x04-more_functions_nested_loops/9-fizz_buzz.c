f8a28100b818cf6b8fbe2d2930a2f60cde7283ca
yaredybk <yb12ybk@gmail.com>
2022-12-15 14:45:17 -0800

    more_functions_nested_loops tasks #ALX

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,8 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line.
- *  But for multiples of three print Fizz instead of the number 
- *  and for the multiples of five print Buzz. 
+ *  But for multiples of three print Fizz instead of the number
+ *  and for the multiples of five print Buzz.
  *  For numbers which are multiples of both three and five print FizzBuzz.
  */
 void main(void)
