56970e1d01d2ea4f7be03f81eb2416daaab32629
yaredybk <yb12ybk@gmail.com>
2022-12-15 07:00:45 -0800

    hot fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,10 @@
 #include"main.h"
 
 /**
- * main - test the function "positive_or_negative" is correct when tested at 0
- * Return: always 0
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 	int i;
