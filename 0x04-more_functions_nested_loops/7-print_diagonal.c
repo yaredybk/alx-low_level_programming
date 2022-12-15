13c387590681e55d0b3d2edb7d6660da338db3e8
yaredybk <yb12ybk@gmail.com>
2022-12-15 14:20:12 -0800

    hot fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,11 +9,14 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < i; j++)
-			_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
-	}
+	if (n > 0)
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	else
+		_printf('\n');
 }
