4d4e719ecc1158ee2bab631a08f4f33c206c78f8
yaredybk <yb12ybk@gmail.com>
2022-12-15 13:50:51 -0800

    hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');	
+			if (j > 9)
+				_putchar(j / 10 +'0');
+			_putchar(j % 10 + '0');	
 		}
 		_putchar('\n');
 	}
