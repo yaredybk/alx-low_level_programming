03d805e724cf421ec6d20dc823c8c4036a91c111
yaredybk <yb12ybk@gmail.com>
2022-12-17 19:17:50 -0800

    hot fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
  * followed by a new line.
  * Return: alwas 0
  */
