987a9daf936694ee12ff747563daf4fadd979178
yaredybk <yb12ybk@gmail.com>
2022-12-19 09:32:32 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: input char
+ */
+void print_rev(char *s)
+{
+	int j;
+
+	j = strlen(s);
+
+	while (j > 0)
+	{
+		j--;
+		_putchar(s[j]);
+	}
+}
