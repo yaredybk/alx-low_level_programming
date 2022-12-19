9996997345a33f174e0d2f852fe7699eceef66d4
yaredybk <yb12ybk@gmail.com>
2022-12-19 10:41:31 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int j;
+	char *s;
+
+	i = strlen(str);
+	s = str;
+
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i + 1) / 2;
+	while (j < i)
+	{
+		_putchar(s[j]);
+	}
+}
