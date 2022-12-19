10065fa906c1ebb4f5e7ba2a612a5d6ab34c3dfd
yaredybk <yb12ybk@gmail.com>
2022-12-19 08:00:00 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ * 
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
