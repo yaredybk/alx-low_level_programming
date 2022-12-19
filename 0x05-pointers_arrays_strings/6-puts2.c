2dc9ac6635c300703a6ad965e66f7ac1b8ef3064
yaredybk <yb12ybk@gmail.com>
2022-12-19 10:12:42 -0800

    hot fix

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * puts2 - prints every other character of a string,
+ * tarting with the first character, followed by a new line.
+ * @str: input string
+ */
+void puts2(char *str)i
+{
+	int i;
+	int j;
+	char *s;
+
+	s = str;
+	i = strlen(str);
+	j = 0;
+
+	while (j < i)
+	{
+		_putchar(s[j]);
+		j += 2;
+	}
