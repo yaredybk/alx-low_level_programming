a4bb8d360d3201a8146eaa80183237142e1914d5
yaredybk <yb12ybk@gmail.com>
2022-12-22 12:04:23 -0800

    fix

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: input string
+ * Return: capitalized string
+ */
+char *string_toupper(char * str)
+{
+	char * i;
+	int j;
+
+	j = 0;
+	i = str;
+
+	while (i[j] != '\0')
+	{
+		if (isalpha(i[j]) && islower(i[j]))
+			i[j] = toupper(i[j]);
+		j++;
+	}
+	return (str);
+}
