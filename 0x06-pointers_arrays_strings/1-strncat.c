42a35f1f77c0a61b9635aff0d38493890d596091
yaredybk <yb12ybk@gmail.com>
2022-12-21 21:07:59 -0800

    fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,3 +9,8 @@
  * @src: second character used as source
  * Return:  a pointer to the resulting string dest
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
