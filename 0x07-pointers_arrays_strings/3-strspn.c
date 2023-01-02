834de7eccd71bdc53de2961906854ed74b4b106d
yaredybk <yb12ybk@gmail.com>
2023-01-02 14:28:52 -0800

    error fix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s:input char
+ * @accept: input char
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
