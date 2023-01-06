c4c4b3cc678993fd45e64071aab766a159c168ab
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-06 03:57:17 +0300

    fix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Returns Length of String
+ * @s: string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
