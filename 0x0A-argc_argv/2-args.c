f46739316eaf07476be10dcc1c6791041bde80b0
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-07 10:19:53 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,15 +11,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++)
-			;
-
-		printf("%d\n", i - 1);
-	}
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
 
 	return (0);
 }
