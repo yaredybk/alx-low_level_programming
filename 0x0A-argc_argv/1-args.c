d2eb4338f770df60476d3f2cc5fbbeafd9506921
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-07 10:14:16 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,8 +4,21 @@
  * main - prints the number of arguments passed into it. followed by a new line
  * @argc: Number of command line arguments
  * @argv: Array name
+ * Return: always 0
  */
-void main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
+	int i;
 
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+
+	return (0);
 }
