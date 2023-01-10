9700c2fd6888f71a23128264563055e3ea3089db
yaredybk <yb12ybk@gmail.com>
2023-01-10 06:21:17 -0800

    fix

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 
 
 #ifndef MAIN_H
@@ -8,4 +8,13 @@
 
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+
+
+
+
+
 #endif
