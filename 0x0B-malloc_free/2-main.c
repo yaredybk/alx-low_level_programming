ed28946665b61fc4a986ebaae4ad46cde88c265f
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-10 22:59:29 +0300

    Update 2-main.c

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -18,28 +18,34 @@ nclude "main.h"
 
 int main(void)
 
+char *str_concat(char *s1, char *s2)
 {
+	char *strnew = NULL;
+	unsigned int i;
+	int n1;
+	int n2;
+	int count;
 
-	    char *s;
-
-
-
-	        s = str_concat("Betty ", "Holberton");
-
-		    if (s == NULL)
-
-			        {
-
-					        printf("failed\n");
-
-						        return (1);
-
-							    }
-
-		        printf("%s\n", s);
-
-			    free(s);
-
-			        return (0);
-
+	count = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (n1 = 0; s1[n1] != '\0'; n1++)
+		;
+	for (n2 = 0; s2[n2] != '\0'; n2++)
+		;
+	strnew = (char *)malloc((n1 + n2 + 1) * sizeof(char));
+	if (strnew == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (; s2[count] != '\0'; i++)
+	{
+		strnew[i] = s2[count];
+		count++;
+	}
+	return (strnew);
 }
