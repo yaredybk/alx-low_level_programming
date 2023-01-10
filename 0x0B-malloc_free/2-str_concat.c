dc2ca9a061538047855ee05de21fe239e62ca9fe
yaredybk <yb12ybk@gmail.com>
2023-01-10 11:16:10 -0800

    fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,29 @@
-#include<main.h>
+#include"main.h"
 
 /**
- * _concat - 
- * char *str_concat(char *s1, char *s2);
+ * str_concat - concatenates two strings.
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer  to a newly allocated space in memor
+ * which contains the contents of s1,
+ * followed by the contents of s2, and null terminated
+ * NULL on failure
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *tmp;
+
+	if (s1 == NULL && s2 == NULL)
+	{
+		tmp = strdup("");
+		return (tmp);
+	}
+	else
+	{
+		tmp = strdup(strcat(s1, s2));
+		if (tmp)
+			return (tmp);
+		else
+			return (NULL);
+	}
+}
