2d7f6afa1a386cab759185ca52a2879a5bbfd06e
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-17 23:41:05 +0300

    hot fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,11 @@ struct my_dog
 	float age;
 	char *owner
 }
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
+
+
 #endif
