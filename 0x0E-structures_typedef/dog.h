cbd10f30345ee85be01a6a538b6a21ad32486f0c
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2023-01-17 23:40:09 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * struct dog - this descripction about which dog
+ * @name: sdfasdf
+ * @age:  asdfas
+ * @owner:  asdf
+ */
 
 #ifndef MAIN_H
 #define MAIN_H
