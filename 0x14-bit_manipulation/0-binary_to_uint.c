68271c8b00d170ba36bdc0253556968cf1622a2f
yaredybk <yb12ybk@gmail.com>
2023-02-22 10:52:08 -0800

    new files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ * there is one or more chars in the
+ * string b that is not 0 or 1 b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int base = 1, i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i + 1])
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		i++;
+	}
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,2 @@
+0x14-bit_manipulationI
+ALX low level programming -> c bit manipulation 
