3447509032048125c8e038bbafe681323b8e853d
yaredybk <yb12ybk@gmail.com>
2023-10-27 09:54:19 -0700

    test

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * main - multiplies two numbers
+ * Return : the multplication of two numbers
+ */
+void main(int argc, char *argv[])
+{
+	int i=argv[1];
+	int j=argv[2];
+	int m=i*j;
+
+	printf("%d\n", m);
+}
