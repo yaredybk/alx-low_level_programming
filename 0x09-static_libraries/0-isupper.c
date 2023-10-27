ea8d19df50c39d1bb6ed3107eee2f27e9d8ec788
yaredybk <yb12ybk@gmail.com>
2023-10-27 15:57:51 -0700

    test

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -8,7 +8,7 @@
  */
 int _isupper(int c)
 {
-	if (isupper(c))
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
 		return (0);
--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -7,9 +7,7 @@
  */
 int _isalpha(int c)
 {
-	int i = isalpha(c);
-
-	if (i)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 		return (1);
 	else
 		return (0);
