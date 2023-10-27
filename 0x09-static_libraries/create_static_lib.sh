217151bd1aa89ff7330692ed26a2e7810fabd4df
yaredybk <yb12ybk@gmail.com>
2023-10-27 16:27:03 -0700

    upload

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#creates a static library called liball.a from all the .c files
+#that are in the current directory.
+
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
+
