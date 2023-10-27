bad00cf9a6d0c72d0a007afd44a65622b0247062
yaredybk <yb12ybk@gmail.com>
2023-10-27 14:46:41 -0700

    test

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include"main.h"
+
+/**
+ * main - add two numbers
+ * @argc: number of arguments passed
+ * @argv: an array of pointers of arguments
+ * Return: Print the result, followed by a new line
+ * If no number is passed to the program, print 0, followed by a new line
+ * If one of the number contains symbols that are not digits, print Error
+ * followed by a new line, and return 1
+ */
+int main(int argc, char *argv[])
+{
+	int s = 0;
+	int ind = 0;
+
+	if (argc < 2)
+		return (printf("0\n"));
+	for (; ind < argc; ind++)
+	{
+
+		if (argv[ind] == '0')
+			continue;
+		int val = 0;
+		val = atoi(argv[ind]);
+		if (val == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		s += val;
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
