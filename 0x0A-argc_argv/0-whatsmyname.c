9efde8eac08c4101d278afc5856cf83c167a1a4c
yaredybk <yb12ybk@gmail.com>
2023-10-27 10:33:32 -0700

    test

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,16 @@
 
 /**
  * main - prints the name of the function
- * Return : 0
+ * @argc: number of arguments passed
+ * @argv: an array of pointers of arguments
+ * Return: 0
  */
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
+	if(argc <0)
+	{
+		return printf("invalid no of arguments");
+	}
 	printf("%s\n", argv[0]);
+	return 0;
 }
