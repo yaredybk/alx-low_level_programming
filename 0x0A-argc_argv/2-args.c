01476615b2014730e4f5c97a16da71e2cec0a8ae
yaredybk <yb12ybk@gmail.com>
2023-10-27 11:44:03 -0700

    test

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,13 +4,13 @@
  * main - prints the name of the function
  * @argc: number of arguments passed
  * @argv: an array of pointers of arguments
- * Return : 0
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	int i=0;
+	int i = 0;
 
-	for(i; i < argc; i++)
+	for (; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
