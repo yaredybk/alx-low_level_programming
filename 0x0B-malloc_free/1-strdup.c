89a2c86b92727d8eafaf0df0ab5febf85207c8fe
yaredybk <yb12ybk@gmail.com>
2023-10-31 21:10:52 -0700

    new files

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * _strdup -  returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: input string to bbe allocated
+ * Return: a pointer to a newly allocated space in memory,
+ * NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	if (str == NULL)
+		return (NULL);
+	return (strdup(str));
+}
+
