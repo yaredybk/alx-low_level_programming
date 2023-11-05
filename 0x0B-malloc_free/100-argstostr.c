a6dc1912a88fc0aaa9c630497d4114f77c2157d4
yaredybk <yb12ybk@gmail.com>
2023-11-05 01:12:08 -0700

    hotfix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,66 +1,56 @@
-#include"main.h"
-
+#include "main.h"
 /**
- * argstostr - function that concatenates all the arguments of your program.
- * @ac: argument count
- * @av: array of arguments
- * Returns a pointer to a new string, or NULL if it fails
- * Each argument should be followed by a \n in the new string
- */
+* argstostr - a function that concatenates all the arguments of your program
+*@ac: count of args passed to the function
+*@av:array of arguments
+*
+*Return: pointer to the new string
+*/
 
 char *argstostr(int ac, char **av)
 {
-	int i;
-	int len;
-	char *p;
+		char *new_string = NULL;
+		int k = 0, i = ac, j, sum = 0, temp = 0;
 
-	len = 0;
-	if (ac == 0 || av == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
-	{
-		int j;
+		if (ac == 0 || av == NULL)
+			return (NULL);
 
-		j = 0;
-		while (av[i][j])
-			len++;
-		len++;
-	}
-	p = (char *)(sizeof(char) * len);
-	if (p == NULL)
-		return (NULL);
-	store(ac, av, p);
-	return (p);
+		while (ac--)
+			sum += (len(av[ac]) + 1);
+		new_string = (char *) malloc(sum + 1);
+
+		if (new_string != NULL)
+		{
+			while (k < i)
+			{
+				for (j = 0; av[k][j] != '\0'; j++)
+					new_string[j + temp] = av[k][j];
+				new_string[temp + j] = '\n';
+				temp += (j + 1);
+				k++;
+			}
+			new_string[temp] = '\0';
+		}
+		else
+		{
+			return (NULL);
+		}
+		return (new_string);
 }
 
-
 /**
- * store - stores arguments in av to p
- * @ac: argument count
- * @av: array of arguments
- * @p: pointer to store chars
- * Return: 0
- */
-int store(int ac, char **av, char *p)
+*len - returns length of str
+*@str: string counted
+*Return: returns the length
+*/
+int len(char *str)
 {
-	int i;
-	int pos;
+		int len = 0;
 
-	pos = 0;
-	i = 0;
-	for (i = 0; i < ac; i++)
-	{
-		int j;
-
-		j = 0;
-		while (av[i][j])
+		if (str != NULL)
 		{
-			p[pos] = av[i][j];
-			j++;
-			pos++;
+			while (str[len])
+				len++;
 		}
-		p[pos] = '\n';
-		pos++;
-	}
-	return (0);
+	return (len);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,14 +7,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
+int len(char *str);
+char *argstostr(int ac, char **av);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 int store(int ac, char **av, char *p);
-
+void print_tab(char **tab);
 
 
 
