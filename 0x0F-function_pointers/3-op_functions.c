c3fc043ec1debaca08a85341bbddfd89b697e75c
yaredybk <yb12ybk@gmail.com>
2023-11-09 21:26:30 -0800

    new

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+/**
+*op_add - the sum of two numbers
+*@a:the first number
+*@b:the second number
+*
+*Return:The sum of a and b
+*/
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+
+/**
+*op_sub - the substraction of two numbers
+*@a:The first number
+*@b:The second number
+*
+*Return: The Difference of a and b
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+*op_mul - multiplication of two numbers
+*@a:The first number
+*@b: The second number
+*
+*Return: The multiplication of a and b
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+*op_div - the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The quotient of a and b.
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+*op_mod - the remainder of the division of two numbers.
+*@a: The first number.
+*@b: The second number.
+*
+*Return: The remainder of the division of a by b.
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+#0x0F. C - Function pointers
+C
+ By: Alexandre Gautier
+ Weight: 1
