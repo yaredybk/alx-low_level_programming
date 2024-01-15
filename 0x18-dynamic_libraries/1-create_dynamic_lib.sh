89bc1e2b392e5841d97fd94cde19618f8392cda3
yaredybk <yb12ybk@gmail.com>
2024-01-15 08:25:51 -0800

    fix

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-#creates a dynamic library called liball.so from all the .c file
-#that are in the current directory.
 gcc -c -fPIC *.c
-gcc -shared -o libdynamic.so *.o
+gcc -shared -o liball.so *.o
